ad5996568cd5d5be0172965b0c0e5951793d9b4f
Tolik Rakhman <tolythedev@gmail.com>
2022-09-13 17:10:48 +0300

    added some css touches, input feb data

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = async () => {
   let feb = data.filter((item) => item.Date.includes("2022-02"));
   let cashFeb = feb.filter((item) => item.Type.includes("CashGame"));
   let cashFebTotal = cashFeb.reduce((sum, item) => sum + item.Amount, 0);
+  cashFebTotal = parseFloat(cashFebTotal.toFixed(2));
   let febTourney = feb.filter((item) => item.Type.includes("Tournament"));
   let febTourneyTotal = febTourney.reduce((sum, item) => sum + item.Amount, 0);
   let febPlayerTransfers = feb.filter((item) =>
@@ -24,6 +25,18 @@ const app = async () => {
   febPlayerTransfers.forEach((item) => {
     febPlayerTransferFrom.add(item.Description);
   });
+
+  let currentCashTotal = document.querySelector(".cash h2 span");
+  currentCashTotal.innerHTML = cashFebTotal;
+
+  let currentTournamentTotal = document.querySelector(".tournaments h2 span");
+  currentTournamentTotal.innerHTML = febTourneyTotal;
+
+  let currentTransferTotal = document.querySelector(".p2p h2 span");
+  currentTransferTotal.innerHTML = febPlayerTransfersAmounts;
+
+  let currentTransferEmails = document.querySelector(".p2p h3 span");
+  currentTransferEmails.innerHTML = [...febPlayerTransferFrom];
 
   console.log("all feb items ");
   console.log(feb);
